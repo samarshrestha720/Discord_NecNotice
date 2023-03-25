7548c5b88dd63d74b0a92ff26515f8ee6a06f8d1
Samar Shrestha <66566474+samarshrestha720@users.noreply.github.com>
2023-03-25 07:56:14 +0000

    fixed a bug where bot keeps sending msg if no image found

--- main.py
+++ main.py
@@ -4,31 +4,28 @@ import csv
 import discord, os, asyncio, aiohttp
 from io import BytesIO
 from keep_alive import keep_alive
-
-
+from time import sleep
 
 client = discord.Client(intents=discord.Intents.default())
 
 
-def wtofile(firstNotice_Title, firstNotice_url,):  #write to file function
-    with open('examNotices.csv', mode='a') as wfile:
-        rows = [firstNotice_Title, firstNotice_url]
-        writer = csv.writer(wfile)
-        writer.writerow(rows)
-        wfile.close()
-
-
-
+def wtofile(
+  firstNotice_Title,
+  firstNotice_url,
+):  #write to file function
+  with open('examNotices.csv', mode='a') as wfile:
+    rows = [firstNotice_Title, firstNotice_url]
+    writer = csv.writer(wfile)
+    writer.writerow(rows)
+    wfile.close()
 
 
 def getImg(url):
   data = requests.get(url, verify=False)
   soup = BeautifulSoup(data.text, 'html.parser')
-  imgBlock = soup.find('figure', {'class': 'wp-block-image size-large'})
+  imgBlock = soup.find('figure', {'class': 'aligncenter size-large'})
   imgUrl = imgBlock.find('a').get('href')
   return imgUrl
-
-  
 
 
 keep_alive()  #make script active
@@ -36,49 +33,61 @@ keep_alive()  #make script active
 
 @client.event
 async def on_ready():
-    await client.wait_until_ready()
-    channel = client.get_channel(int(os.environ['channel_id']))
-    print(f"Logged in as {client.user}")
-    count = 0
-    while True:
-        data = requests.get("https://nec.edu.np/exam/?cat=3", verify=False)
-        soup = BeautifulSoup(data.text, 'html.parser')
-        firstNotice = soup.find('div', {'class': 'col-sm-9 col-md-9'})
-        firstNotice_url = firstNotice.find('a').get('href')
-        firstNotice_Title = firstNotice.find('a').find('h4').text.strip()
+  await client.wait_until_ready()
+  channel = client.get_channel(int(os.environ['channel_id']))
+  print(f"Logged in as {client.user}")
+  count = 0
+  while True:
+    data = requests.get("https://nec.edu.np/exam/?cat=3", verify=False)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    firstNotice = soup.find('div', {'class': 'col-sm-9 col-md-9'})
+    firstNotice_url = firstNotice.find('a').get('href')
+    firstNotice_Title = firstNotice.find('a').find('h4').text.strip()
 
-        with open('examNotices.csv', mode='r') as file:
-            csvFile = csv.reader(file)
-            flag = 0
-            for lines in csvFile:
-                if (lines[0] == firstNotice_Title):
-                    print('NO NEW NOTICES!!')
-                    flag = 1
-                    break
-            if (flag == 0):
-                print('NEW NOTICE ALERT!!')
-                print("Title: ", firstNotice_Title)
-                imgUrl = getImg(firstNotice_url)  #get img url
-              
-                try:
-                  async with aiohttp.ClientSession() as session:  # creates session
-                    async with session.get(imgUrl,verify_ssl=False) as resp:  # gets image from url
-                        img = await resp.read()  # reads image from response
-                        with BytesIO(img) as file:  # converts to file-like object
-                          await channel.send(
-                    f"**_nec_ Exam Divison Notice:**\n{firstNotice_Title}\n{firstNotice_url}\n-NotifyNotice\n",file=discord.File(file, "testimage.png"))
-                except:
-                  os.system("kill 1")
-  
-                wtofile(firstNotice_Title, firstNotice_url)
-                          
-            file.close()
-        count += 1
-        print(f"count: {count}")
-        await asyncio.sleep(60)
+    with open('examNotices.csv', mode='r') as file:
+      csvFile = csv.reader(file)
+      flag = 0
+      for lines in csvFile:
+        if (lines[0] == firstNotice_Title):
+          print('NO NEW NOTICES!!')
+          flag = 1
+          break
+      if (flag == 0):
+        print('NEW NOTICE ALERT!!')
+        print("Title: ", firstNotice_Title)
+        try:
+          imgUrl = getImg(firstNotice_url)  #get img url
+        except:
+          await channel.send(
+            f"**_nec_ Exam Divison Notice:**\n{firstNotice_Title}\n{firstNotice_url}\n-NotifyNotice\nCould not get Img"
+          )
+          print("Discord Notice sent Sucessfully without img!!")
+          wtofile(firstNotice_Title, firstNotice_url)
+          sleep(1)
+          os.system("kill 1")
+
+        try:
+          async with aiohttp.ClientSession() as session:  # creates session
+            async with session.get(
+                imgUrl, verify_ssl=False) as resp:  # gets image from url
+              img = await resp.read()  # reads image from response
+              with BytesIO(img) as file:  # converts to file-like object
+                await channel.send(
+                  f"**_nec_ Exam Divison Notice:**\n{firstNotice_Title}\n{firstNotice_url}\n-NotifyNotice\n",
+                  file=discord.File(file, "testimage.png"))
+                print("Discord Notice sent Sucessfully with img!!")
+        except:
+          os.system("kill 1")
+
+        wtofile(firstNotice_Title, firstNotice_url)
+
+      file.close()
+    count += 1
+    print(f"count: {count}")
+    await asyncio.sleep(60)
 
 
 try:
-    client.run(os.environ['disToken'])
+  client.run(os.environ['disToken'])
 except:
-    os.system("kill 1")
+  os.system("kill 1")
